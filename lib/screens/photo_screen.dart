365509e0fb424c51a3db5daceed6909d2e022aef
Mikalai Beskaravainy <inwm07@gmail.com>
2020-05-18 21:09:59 +0300

    staggered animations

--- lib/screens/photo_screen.dart
+++ lib/screens/photo_screen.dart
@@ -4,7 +4,7 @@ import 'package:flutter/widgets.dart';
 import '../widgets/widgets.dart';
 import '../res/res.dart';
 
-class FullScreenImage extends StatelessWidget {
+class FullScreenImage extends StatefulWidget {
   String altDescription;
   String userName;
   String name;
@@ -16,6 +16,32 @@ class FullScreenImage extends StatelessWidget {
     this.altDescription = altDescription ?? 'text...';
     this.photo = photo ??
         'https://flutter.dev/assets/404/dash_nest-c64796b59b65042a2b40fae5764c13b7477a592db79eaf04c86298dcb75b78ea.png';
+  }
+
+  @override
+  _FullScreenImageState createState() => _FullScreenImageState();
+}
+
+class _FullScreenImageState extends State<FullScreenImage> with TickerProviderStateMixin {
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(duration: const Duration(milliseconds: 1500), vsync: this);
+    _playAnimation();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  Future<void> _playAnimation() async {
+    try {
+      await _controller.forward().orCancel;
+    } on TickerCanceled {}
   }
 
   @override
@@ -33,35 +59,46 @@ class FullScreenImage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Photo(photoLink: photo),
+            Photo(photoLink: widget.photo),
             Padding(
               padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
               child: Text(
-                altDescription,
+                widget.altDescription,
                 maxLines: 3,
                 overflow: TextOverflow.ellipsis,
                 style: AppStyles.h3,
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-              child: Row(
-                children: <Widget>[
-                  UserAvatar('https://skill-branch.ru/img/speakers/Adechenko.jpg'),
-                  SizedBox(width: 6.0),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
+            AnimatedBuilder(
+              animation: _controller,
+              builder: (BuildContext context, Widget child) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                  child: Row(
                     children: <Widget>[
-                      Text(name, style: AppStyles.h1Black),
-                      Text(
-                        '@' + userName,
-                        style: AppStyles.h5Black.copyWith(color: AppColors.manatee),
+                      Opacity(
+                        opacity: buildAnimationUserAvatar(),
+                        child: UserAvatar('https://skill-branch.ru/img/speakers/Adechenko.jpg'),
+                      ),
+                      SizedBox(width: 6.0),
+                      Opacity(
+                        opacity: buildAnimationUserMeta(),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(widget.name, style: AppStyles.h1Black),
+                            Text(
+                              '@' + widget.userName,
+                              style: AppStyles.h5Black.copyWith(color: AppColors.manatee),
+                            ),
+                          ],
+                        ),
                       ),
                     ],
                   ),
-                ],
-              ),
+                );
+              },
             ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
@@ -83,6 +120,28 @@ class FullScreenImage extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  double buildAnimationUserMeta() {
+    return Tween<double>(begin: 0.0, end: 1.0)
+        .animate(
+          CurvedAnimation(
+            parent: _controller,
+            curve: Interval(0.5, 1.0, curve: Curves.ease),
+          ),
+        )
+        .value;
+  }
+
+  double buildAnimationUserAvatar() {
+    return Tween<double>(begin: 0.0, end: 1.0)
+        .animate(
+          CurvedAnimation(
+            parent: _controller,
+            curve: Interval(0.0, 0.5, curve: Curves.ease),
+          ),
+        )
+        .value;
   }
 
   Widget _buildButton({String txt = 'Button'}) {
